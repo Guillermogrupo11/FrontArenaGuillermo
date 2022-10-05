a3765f8eb80e6f0d1b79dfdcd9bb59a5046115a6
Guillermo <guillermo.arena@gmail.com>
2022-10-05 19:43:12 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  URL: 'http://localhost:8080',
-  //URL: 'http://heroku-backendgda.herokuapp.com/'
+  //URL: 'http://localhost:8080',
+  URL: 'https://portfoliobackendgda.herokuapp.com/'
 };
 
 /*
